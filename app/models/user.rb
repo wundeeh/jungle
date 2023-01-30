5abaa205d6e2f6302582446fe0cc70a730406911
wundeeh <jakiriksh@gmail.com>
2023-01-30 23:15:34 +0000

    user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
 
   has_secure_password
+
+  validates_uniqueness_of :email
+
+  validates_length_of :password, :minimum => 5
   
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,66 @@
 require 'rails_helper'
 
-# RSpec.describe User, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe User, type: :model do
+  
+  describe "Validations" do
+    it "is valid with valid attributes" do
+      @user = User.new(
+        name: "Landon",
+        email: "test@test.com",
+        password: "VeryC00lPassword",
+        password_confirmation: "VeryC00lPassword"
+      )
+
+      @user.save
+
+      expect(@user).to be_valid
+    end
+
+    it "is invalid when password confirmation is wrong" do
+      @user = User.new(
+        name: "Landon",
+        email: "test@test.com",
+        password: "VeryC00lPassword",
+        password_confirmation: "WrongPassword"
+      )
+
+      @user.save
+
+      expect(@user).to_not be_valid
+    end
+
+    it "is invalid when there's a repeating email" do
+      @user = User.new(
+        name: "Landon",
+        email: "test@test.com",
+        password: "VeryC00lPassword",
+        password_confirmation: "VeryC00lPassword"
+      )
+      @user.save
+
+      @u = User.new(
+        name: "Landon",
+        email: "test@test.com",
+        password: "VeryC00lPassword",
+        password_confirmation: "VeryC00lPassword"
+      )
+
+      @u.save
+      
+      expect(@u).to_not be_valid
+
+    end
+
+    it "is invalid if the password is shorter than 5 characters" do
+      @user = User.new(
+        name: "Landon",
+        email: "test@test.com",
+        password: "ohNo",
+        password_confirmation: "ohNo"
+      )
+      @user.save
+
+      expect(@user).to_not be_valid
+    end
+  end
+end
