e547a39685e9eec8ca186601f45a559fdf19559e
wundeeh <jakiriksh@gmail.com>
2023-01-30 22:13:25 +0000

    add tests for product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,9 +1,79 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+  
   describe 'Validations' do
     it "is valid with valid attributes" do
-      expect(Product.new).to be_valid
+      @product = Product.new
+      @cat = Category.new
+      @cat.name = "CatName"
+      @product.name = "Landon"
+      @product.price_cents = 15000
+      @product.quantity = 1
+      @product.category = @cat
+      @product.save
+
+      expect(@product.valid?).to be true
+    end
+
+    it "is invalid when the name is nil" do
+      @product = Product.new
+      @cat = Category.new
+      @cat.name = "CatName"
+      @product.name = nil
+      @product.price_cents = 15000
+      @product.quantity = 1
+      @product.category = @cat
+      @product.save
+      
+      expect(@product).to_not be_valid
+      expect(@product.errors[:name]).to include("can't be blank")
+    end
+
+    it "is invalid when the price is nil" do
+      @product = Product.new
+      @cat = Category.new
+      @cat.name = "CatName"
+      @product.name = "Landon"
+      @product.price_cents = nil
+      @product.quantity = 1
+      @product.category = @cat
+      @product.save
+
+      expect(@product).to_not be_valid
+      expect(@product.errors[:price_cents]).to include("is not a number")
+    end
+
+    it "is invalid when the quantity is nil" do
+      @product = Product.new
+      @cat = Category.new
+      @cat.name = "CatName"
+      @product.name = "Landon"
+      @product.price_cents = 15000
+      @product.quantity = nil
+      @product.category = @cat
+      @product.save
+
+      expect(@product).to_not be_valid
+      expect(@product.errors[:quantity]).to include("can't be blank")
+    end
+
+    it "is invalid when the category is nil" do
+      @product = Product.new
+      @cat = Category.new
+      @cat.name = "CatName"
+      @product.name = "Landon"
+      @product.price_cents = 15000
+      @product.quantity = 1
+      @product.category = nil
+      @product.save
+
+      puts "-----"
+      puts @product.errors.full_messages
+      puts "-----"
+
+      expect(@product).to_not be_valid
+      expect(@product.errors[:category]).to include("can't be blank")
     end
   end
 end
